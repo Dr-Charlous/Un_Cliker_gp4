4c2927ca0efe136f559a34f6bde7e5bff42f793f
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2022-12-10 17:57:57 +0100

    shop

--- Assets/Script/Maingame.cs
+++ Assets/Script/Maingame.cs
@@ -12,9 +12,26 @@ public class Maingame : MonoBehaviour
     public float scoreIncreasePerSecond;
     public float x;
 
+    //SHOP
+    public int shop1prize;
+    public Text shop1text;
+
+    public int shop2prize;
+    public Text shop2text;
+
+    //AMOUNT
+    public Text amount1Text;
+    public int amount1;
+    public float amount1Profit;
+
+    public Text amount2Text;
+    public int amount2;
+    public float amount2Profit;
+
     //Use this for init
     void Start()
     {
+        //CLIKER
         currentScore = 0;
         hitPower = 1;
         scoreIncreasePerSecond = 1;
@@ -24,13 +41,47 @@ public class Maingame : MonoBehaviour
     //Update per frame
     void Update()
     {
-        scoreText.text = (int)currentScore + "$";
+        //CLIKER
+        scoreText.text = "Money : " + (int)currentScore + " $";
         scoreIncreasePerSecond = x * Time.deltaTime;
         currentScore = currentScore + scoreIncreasePerSecond;
+
+        //SHOP
+        shop1text.text = "Tier 1: " + shop1prize + " $";
+        shop2text.text = "Tier 2: " + shop2prize + " $";
+
+        //AMOUNT
+        amount1Text.text = "Tier 1: " + amount1 + " arts $:" + amount1Profit + "/s";
+        amount2Text.text = "Tier 2: " + amount2 + " arts $:" + amount2Profit + "/s";
     }
 
     public void Hit()
     {
+        //CLIKER
         currentScore += hitPower;
+    }
+
+    public void Shop1()
+    {
+        if(currentScore>=shop1prize)
+        {
+            currentScore -= shop1prize;
+            amount1 += 1;
+            amount1Profit += 1;
+            x += 1;
+            shop1prize += 25;
+        }
+    }
+
+    public void Shop2()
+    {
+        if (currentScore >= shop2prize)
+        {
+            currentScore -= shop2prize;
+            amount2 += 1;
+            amount2Profit += 5;
+            x += 5;
+            shop2prize += 125;
+        }
     }
 }
