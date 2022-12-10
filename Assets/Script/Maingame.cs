b26657967de174380a3f8824bddf2eda70333b73
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2022-12-10 14:56:23 +0100

    Enfin productif -_-

--- Assets/Script/Maingame.cs
+++ Assets/Script/Maingame.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Maingame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //CLIKER
+    public Text scoreText;
+    public float currentScore;
+    public float hitPower;
+    public float scoreIncreasePerSecond;
+    public float x;
+
+    //Use this for init
     void Start()
     {
-        
+        currentScore = 0;
+        hitPower = 1;
+        scoreIncreasePerSecond = 1;
+        x = 0f;
     }
 
-    // Update is called once per frame
+    //Update per frame
     void Update()
     {
-        
+        scoreText.text = (int)currentScore + "$";
+        scoreIncreasePerSecond = x * Time.deltaTime;
+        currentScore = currentScore + scoreIncreasePerSecond;
+    }
+
+    public void Hit()
+    {
+        currentScore += hitPower;
     }
 }
