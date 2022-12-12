422308be03304e792897bbb2d3a8b8f2fa92ec29
Dr-Charlous <charles.piercourt@outlook.fr>
2022-12-12 17:59:55 +0100

    corection

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -6,13 +6,21 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public GameObject ButtonStart;
+    public GameObject ButtonCredit;
     public GameObject ButtonQuit;
+
     public void OnClickPlay()
     {
         SceneManager.LoadScene("Maingame");
     }
+
     public void OnClickQuit()
     {
         Application.Quit();
+    }
+
+    public void OnClickCredit()
+    {
+        SceneManager.LoadScene("Credit");
     }
 }
