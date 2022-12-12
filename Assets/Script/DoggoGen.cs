5555ea4ef2decf4c4c511dccad8325653048fd4e
Dr-Charlous <charles.piercourt@outlook.fr>
2022-12-12 15:52:22 +0100

    les cages

--- Assets/Script/DoggoGen.cs
+++ Assets/Script/DoggoGen.cs
@@ -7,6 +7,7 @@ public class DoggoGen : MonoBehaviour
     //DOGS
     public List<GameObject> myDogList = new List<GameObject>();
     public List<GameObject> myDogHList = new List<GameObject>();
+    public List<GameObject> myDogCageList = new List<GameObject>();
     private int t = 0;
     public int tMax = 200;
 
@@ -16,16 +17,21 @@ public class DoggoGen : MonoBehaviour
 
         if (t >= tMax)
         {
-            int i = Random.Range(0, 2);
+            int i = Random.Range(0, 3);
             if (i == 0)
             {
                 GameObject spawned = Instantiate(myDogList[Random.Range(0, myDogList.Count)], new Vector3(-12, 0, 0), Quaternion.identity);
                 spawned.transform.SetParent(this.transform);
                 spawned.AddComponent<Dog>();
             }
-            else
+            else if(i == 1)
             {
                 GameObject spawned = Instantiate(myDogHList[Random.Range(0, myDogHList.Count)], new Vector3(-12, 0.5f, 0), Quaternion.identity);
+                spawned.transform.SetParent(this.transform);
+                spawned.AddComponent<Dog>();
+            } else
+            {
+                GameObject spawned = Instantiate(myDogCageList[Random.Range(0, myDogCageList.Count)], new Vector3(-12, 2, 0), Quaternion.identity);
                 spawned.transform.SetParent(this.transform);
                 spawned.AddComponent<Dog>();
             }
