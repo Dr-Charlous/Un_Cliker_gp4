e9a1b53308e897f44e6329c3002f8cab4c6ee143
Dr-Charlous <charles.piercourt@outlook.fr>
2022-12-13 10:37:47 +0100

    efdqsed

--- Assets/Script/DoggoGen.cs
+++ Assets/Script/DoggoGen.cs
@@ -9,7 +9,7 @@ public class DoggoGen : MonoBehaviour
     public List<GameObject> myDogHList = new List<GameObject>();
     public List<GameObject> myDogCageList = new List<GameObject>();
     private int t = 0;
-    public int tMax = 200;
+    public int tMax = Screen.width+5;
 
     public void DoggoSpawn()
     {
@@ -20,18 +20,18 @@ public class DoggoGen : MonoBehaviour
             int i = Random.Range(0, 3);
             if (i == 0)
             {
-                GameObject spawned = Instantiate(myDogList[Random.Range(0, myDogList.Count)], new Vector3(-12, -2.1f, 0), Quaternion.identity);
+                GameObject spawned = Instantiate(myDogList[Random.Range(0, myDogList.Count)], new Vector3(-12, -2.45f, 0), Quaternion.identity);
                 spawned.transform.SetParent(this.transform);
                 spawned.AddComponent<Dog>();
             }
             else if(i == 1)
             {
-                GameObject spawned = Instantiate(myDogHList[Random.Range(0, myDogHList.Count)], new Vector3(-12, -1.9f, 0), Quaternion.identity);
+                GameObject spawned = Instantiate(myDogHList[Random.Range(0, myDogHList.Count)], new Vector3(-12, -2.25f, 0), Quaternion.identity);
                 spawned.transform.SetParent(this.transform);
                 spawned.AddComponent<Dog>();
             } else
             {
-                GameObject spawned = Instantiate(myDogCageList[Random.Range(0, myDogCageList.Count)], new Vector3(-12, -1.2f, 0), Quaternion.identity);
+                GameObject spawned = Instantiate(myDogCageList[Random.Range(0, myDogCageList.Count)], new Vector3(-12, -1.9f, 0), Quaternion.identity);
                 spawned.transform.SetParent(this.transform);
                 spawned.AddComponent<Dog>();
             }
